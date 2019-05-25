91cdb5cba1c30cde4bea1e907f5795017a223859
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-05-25 18:32:37 +0200

    Added config

---
+++ Nemesis/Config.cpp
@@ -0,0 +1,36 @@
+#include "Config.hpp"
+
+Config::Config(const std::string_view app_name, const std::string_view file_name)
+{
+	this->app_name = app_name;
+	this->file_name = file_name;
+}
+
+Config::~Config() = default;
+
+auto Config::Set(std::string_view key, std::string_view value) const -> BOOL
+{
+	return WritePrivateProfileString(app_name.data(), key.data(), value.data(), file_name.data());
+}
+
+auto Config::Get(std::string_view key) const -> std::string
+{
+	std::string buffer (MAX_PATH, '\0');
+	DWORD characters_copied;
+
+	//
+	// Get the string from the config
+	//
+	if((characters_copied = GetPrivateProfileString(app_name.data(), key.data(), nullptr, buffer.data(), buffer.size(), file_name.data())) <= 0)
+	{
+		return std::string();
+	}
+	
+	//
+	// Resize the buffer
+	//
+	buffer.resize(characters_copied);
+
+	return buffer;
+}
+
---
+++ Nemesis/Config.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+#include <Windows.h>
+
+
+class Config
+{
+	/**
+	 * \brief The name of the application.
+	 */
+	std::string_view app_name;
+	
+	/**
+	 * \brief The path to the config file.
+	 */
+	std::string_view file_name;
+
+public:
+
+	/**
+	 * \brief Initializes the variables.
+	 * \param app_name the application name
+	 * \param file_name the config path
+	 */
+	explicit Config(std::string_view app_name, std::string_view file_name);
+
+	/**
+	 * \brief The default destructor.
+	 */
+	~Config();
+
+	/**
+	 * \brief Sets the key and value in the config.
+	 * \param key the specified key
+	 * \param value the specified value for the key
+	 * \return nonzero if successful
+	 */
+	auto Set(std::string_view key, std::string_view value) const -> BOOL;
+
+	/**
+	 * \brief Gets the value of the key from the config.
+	 * \param key the key of the value
+	 * \return the value
+	 */
+	auto Get(std::string_view key) const -> std::string;
+};
